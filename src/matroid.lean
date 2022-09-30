ebfdb9b25c1a397c7b21bc698483c02b15a836f4
wydra1 <44512814+wydra1@users.noreply.github.com>
2022-09-30 14:54:39 +0100

    Commit 5.2

--- src/matroid.lean
+++ src/matroid.lean
@@ -1,8 +1,9 @@
 import combinatorics.simple_graph.basic
 import data.finite
 import data.finset.basic
-import data.quot
+import data.quot,
 import data.order,
+import data.set,
 universes u
 
 open finset
@@ -38,16 +39,32 @@ variables (M : matroid α) [decidable_pred M.ℐ]
 /- A subset of `E` that is not in `ℐ` is called dependent. -/
 def dependent_sets : finset (finset α) := filter (λ s, ¬ M.ℐ s) univ.powerset
 
+lemma dep_not_ind (C₁ : finset α): C₁ ∈ M.dependent_sets ↔ ¬M.ℐ C₁ :=
+begin
+  split,
+  {intro h,
+  unfold dependent_sets at h,
+  have h': C₁ ∈ univ.powerset ∧ ¬ M.ℐ C₁,
+  {rw mem_filter at h,
+  exact h,},
+  cases h',
+  contradiction,},
+  {intro h,
+  unfold dependent_sets,
+  rw mem_filter,
+  split,
+  {simp,},
+  {exact h,}}
+end
+
 -- (C1)
 lemma empty_not_dependent : ∅ ∉ M.dependent_sets :=
 begin
   have h: M.ℐ ∅, exact M.empty,
   intro h',
-  unfold dependent_sets at h',
-  have h'': ∅ ∈ univ.powerset ∧ ¬ M.ℐ ∅,
-  {rw mem_filter at h',
+  have h'':¬ M.ℐ ∅,
+  {rw <- dep_not_ind,
   exact h',},
-  cases h'',
   contradiction,
 end
 variables [decidable_pred (λ (D : finset α), can_exchange (λ (_x : finset α), _x ∈ D.powerset.erase D))]
@@ -63,17 +80,21 @@ begin
   simp,
   split,
   {intro h,
-  split,
-  {exact finset.mem_of_mem_filter C₁ h,},
+  split, {exact finset.mem_of_mem_filter C₁ h,},
   {intros C g,
   have C₁_dep : C₁∈ M.dependent_sets, exact finset.mem_of_mem_filter C₁ h,
   unfold dependent_sets at C₁_dep,
   rw mem_filter at C₁_dep,
   cases C₁_dep with _ C₁_dep,
-  sorry,},-- C₁ is minimal dependent so C is ind.
-  },
-  {intro h,
+  unfold circuit at h,
+  have h': C∈ C₁.ssubsets,
+  {rw mem_ssubsets,
+  exact g,},
+  rw mem_filter at h,
   cases h with h₁ h₂,
+  specialize h₂ C h',
+  exact h₂,}},
+  {rintros ⟨h₁, h₂⟩,
   unfold circuit, 
   rw mem_filter,
   split, exact h₁,
@@ -100,14 +121,20 @@ begin
   {rw mem_ssubsets,
   exact h',},
   specialize h₂ C₁ h'',
-  
+  rw dep_not_ind at C₁_dep,
+  contradiction,
 end
 /- `(C3)` If C₁ and C₂ are distinct members of M.circuit and e ∈ C₁ ∩ C₂, then
 there is a member C₃ of M.circuit such that C₃ ⊆ (C₁ ∪ C₂) - e.   -/
 lemma circuit_dependence (C₁ C₂ : finset α) (h₁ : C₁ ∈ M.circuit) (h₂ : C₂ ∈ M.circuit) (h : C₁ ≠ C₂) (e : α) :
   e ∈ C₁ ∩ C₂ → ∃ C₃ ∈ M.circuit, C₃ ⊆ (C₁ ∪ C₂) \ {e} :=
 begin
-  sorry,
+  intro e_in,
+  by_contra j,
+  push_neg at j,
+
+
+
 end
 
 
@@ -115,15 +142,14 @@ end
 -- is a function where ∀ y ⊆ x, p x → p y, then there are "maximal" x : finset α
 -- or maybe we can define this w.r.t rank function?
 /- A maximal member of `ℐ` is called a basis. -/
--- def bases : finset (finset α) := filter (λ s, M.ℐ s) univ.powerset
+def bases : finset (finset α) := filter (λ s, M.ℐ s) univ.powerset
 
 
 -- this is broken because of some missing instance somewhere, lean doesn't seem to
 -- get that if V is finite then a simple graph on V will have finite edge set so
 -- uncomment at your own peril (jk if you're feeling up for the challenge you can
 -- probably figure out how to fix it)
-/- variables (V : Type u) (G : simple_graph V) [fintype V] [decidable_eq V] [fintype G.edge_set]
-
+variables (V : Type u) (G : simple_graph V) [fintype V] [decidable_eq V] [fintype G.edge_set]
 namespace simple_graph
 
 instance : fintype G.edge_set :=
@@ -132,17 +158,17 @@ instance : fintype G.edge_set :=
 
 end simple_graph
 
-def graphic_matroid (G : simple_graph V) : matroid G.edge_set := _-/
+def graphic_matroid (G : simple_graph V) : matroid G.edge_set := _
 
 
 
 -- coercing x from fintype ↑s to fintype α is proving very annoying
-/- @[ext]
+@[ext]
 def restriction {V : Type u} (M : matroid α) (s : finset α) : matroid s :=
 { ℐ := λ x, M.ℐ _,
   empty := _,
   hereditary := _,
-  ind := _ }-/
+  ind := _ }
 
 
 
