7f10ad7600b77ade8913c931cacc54faef28a414
wydra1 <44512814+wydra1@users.noreply.github.com>
2022-09-30 14:30:45 +0100

    Commit 5.1

--- src/matroid.lean
+++ src/matroid.lean
@@ -1,6 +1,8 @@
 import combinatorics.simple_graph.basic
 import data.finite
-
+import data.finset.basic
+import data.quot
+import data.order,
 universes u
 
 open finset
@@ -41,38 +43,64 @@ lemma empty_not_dependent : ∅ ∉ M.dependent_sets :=
 begin
   have h: M.ℐ ∅, exact M.empty,
   intro h',
-  have h'': ∅ ∈ univ.powerset ∧ ¬ M.ℐ ∅ ,
-  sorry,
+  unfold dependent_sets at h',
+  have h'': ∅ ∈ univ.powerset ∧ ¬ M.ℐ ∅,
+  {rw mem_filter at h',
+  exact h',},
+  cases h'',
+  contradiction,
 end
 variables [decidable_pred (λ (D : finset α), can_exchange (λ (_x : finset α), _x ∈ D.powerset.erase D))]
 
 def circuit : finset (finset α) :=
-  finset.filter (λ (D : finset α), (∀ (S ∈ (erase D.powerset D)), M.ℐ S)) (M.dependent_sets)
+  finset.filter (λ (D : finset α), (∀ (S ∈ (D.ssubsets)), M.ℐ S)) (M.dependent_sets)
 
 
 @[simp]
 lemma mem_circuit (C₁ : finset α) :
-  C₁ ∈ M.circuit ↔ C₁ ∈ M.dependent_sets ∧ (∀ (C₂ ∈ (erase C₁.powerset C₁)), M.ℐ C₂) :=
+  C₁ ∈ M.circuit ↔ C₁ ∈ M.dependent_sets ∧ (∀ (C ∈ (C₁.ssubsets)), M.ℐ C) :=
 begin
   simp,
   split,
   {intro h,
   split,
   {exact finset.mem_of_mem_filter C₁ h,},
-  {intro C₂,
-  intros g g',
-  have C₁_dep : C₁∈ M.dependent_sets, exact finset.mem_of_mem_filter C₁ h,},
+  {intros C g,
+  have C₁_dep : C₁∈ M.dependent_sets, exact finset.mem_of_mem_filter C₁ h,
+  unfold dependent_sets at C₁_dep,
+  rw mem_filter at C₁_dep,
+  cases C₁_dep with _ C₁_dep,
+  sorry,},-- C₁ is minimal dependent so C is ind.
   },
   {intro h,
   cases h with h₁ h₂,
-  sorry,}
+  unfold circuit, 
+  rw mem_filter,
+  split, exact h₁,
+  {intros S h',
+  have S_sub_C₁ : S ⊂ C₁,
+  {rw mem_ssubsets at h',
+  exact h',},
+  specialize h₂ S S_sub_C₁,
+  exact h₂,}}
 end
 
 /- `(C2)` if C₁ and C₂ are members of C and C₁ ⊆ C₂, then C₂ = C₂.
 In other words, C forms an antichain. -/
 lemma circuit_antichain (C₁ C₂ : finset α) (h₁ : C₁ ∈ M.circuit) (h₂ : C₂ ∈ M.circuit) : C₁ ⊆ C₂ → C₁ = C₂ :=
 begin
-  sorry,
+  intro g,
+  rw mem_circuit at h₂,
+  rw mem_circuit at h₁,
+  cases h₁ with C₁_dep h₁,
+  cases h₂ with C₂_dep h₂,
+  by_contradiction,
+  have h' : C₁⊂ C₂, exact ssubset_of_subset_of_ne g h,
+  have h'': C₁∈ C₂.ssubsets,
+  {rw mem_ssubsets,
+  exact h',},
+  specialize h₂ C₁ h'',
+  
 end
 /- `(C3)` If C₁ and C₂ are distinct members of M.circuit and e ∈ C₁ ∩ C₂, then
 there is a member C₃ of M.circuit such that C₃ ⊆ (C₁ ∪ C₂) - e.   -/
